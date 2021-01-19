7e092304314999667495b65d80f1638d5b01273d
Mahi Satyanarayana <msatya@appriver.com>
2021-01-19 02:49:45 -0600

    Terraforming sb-namespace, sb-queue, sb-auth-rule and finally adding sb-connectionstring to function-app

--- IaC/Terraform/main.tf
+++ IaC/Terraform/main.tf
@@ -29,7 +29,7 @@ resource "azurerm_app_service_plan" "main" {
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   kind                = "FunctionApp"
-
+  
   sku {
     tier = "Dynamic"
     size = "Y1"
@@ -43,8 +43,44 @@ resource "azurerm_function_app" "main" {
   app_service_plan_id       = azurerm_app_service_plan.main.id
   storage_account_name      = azurerm_storage_account.main.name
   storage_account_access_key = azurerm_storage_account.main.primary_access_key
+  version                    = "~3"
+
+  site_config {
+    ftps_state          = "Disabled"
+  }
 
   app_settings = {
     AppInsights_InstrumentationKey = azurerm_application_insights.main.instrumentation_key
+    SERVICEBUS_CONNECTION_STRING = azurerm_servicebus_queue_authorization_rule.main.primary_connection_string
   }
+}
+
+resource "azurerm_servicebus_namespace" "main" {
+  name                = "${var.prefix}sb"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  sku                 = "Standard"
+
+}
+
+resource "azurerm_servicebus_queue" "main" {
+  name                = "whosoff"
+  resource_group_name = azurerm_resource_group.main.name
+  namespace_name      = azurerm_servicebus_namespace.main.name
+
+  enable_partitioning = true
+  default_message_ttl = "PT60S"
+  dead_lettering_on_message_expiration = true
+  requires_duplicate_detection = true
+}
+
+resource "azurerm_servicebus_queue_authorization_rule" "main" {
+  name                = "SendAndListen"
+  namespace_name      = azurerm_servicebus_namespace.main.name
+  queue_name          = azurerm_servicebus_queue.main.name
+  resource_group_name = azurerm_resource_group.main.name
+
+  listen = true
+  send   = true
+  manage = false
 }
